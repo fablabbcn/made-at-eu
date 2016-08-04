05485cfa7bd2ec7fa037abacbe2a095de1251147
mqvlm <m.quintero09@gmail.com>
2016-08-04 18:18:05 +0200

    what y logo responsiveness

--- js/logoHeader.js
+++ js/logoHeader.js
@@ -3,11 +3,11 @@
       if ($(document).scrollTop() == 0) {
         $('#logo-navbar').removeClass('logo-navbar-min');
         $('#logo-navbar').addClass('logo-max');
-        $('.logo-box img').attr('src', '/img/logoFabCity.png');
+        $('.logo-box img').attr('src', '/img/logo-made.png');
       } else {
         $('#logo-navbar').removeClass('logo-max');
         $('#logo-navbar').addClass('logo-navbar-min');
-        $('.logo-box img').attr('src', '/img/logo-min.png');
+        /*$('.logo-box img').attr('src', '/img/logo-min.png');*/
       }
     }); 
 
