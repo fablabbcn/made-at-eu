a34462622de1e4fa8b385e29b0ecb6863dbff51f
m <m.quintero09@gmail.com>
2017-02-07 16:46:07 +0100

    reverse tweak test

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,12 +2,8 @@
 /*Start Bootstrap Carousel Slider 
 https://blackrockdigital.github.io/startbootstrap-full-slider/*/
 
-$(window).load(function() {
-
-	$('.carousel').carousel({
-	    interval: 3000 //changes the speed
-	})
-
+$('.carousel').carousel({
+    interval: 3000 //changes the speed
 })
 
 
