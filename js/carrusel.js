4e1a8a08808360a3698329280dfad36f9ac2fd27
m <m.quintero09@gmail.com>
2017-02-07 16:44:06 +0100

    carousel tweak test
    move only when everything is downloaded

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,8 +2,12 @@
 /*Start Bootstrap Carousel Slider 
 https://blackrockdigital.github.io/startbootstrap-full-slider/*/
 
-$('.carousel').carousel({
-    interval: 3000 //changes the speed
+$(window).load(function() {
+
+	$('.carousel').carousel({
+	    interval: 3000 //changes the speed
+	})
+
 })
 
 
